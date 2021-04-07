e505cfeb7c1fd8b262f40a095a34819efef4e71a
void <lehelkov35@gmail.com>
2021-04-07 21:44:46 +0300

    Board and Card kickstart

--- include/board.h
+++ include/board.h
@@ -1,4 +1,16 @@
 #ifndef SAPITRELLO_CSOPORT_11_BOARD_H
 #define SAPITRELLO_CSOPORT_11_BOARD_H
 
+#include "defines.h"
+#include "card.h"
+
+typedef struct {
+    char* name;
+
+    int numberOfCards;
+    Card* cards;    // Linked list
+    int numberOfUsers;
+    char** users;
+} Board;
+
 #endif //SAPITRELLO_CSOPORT_11_BOARD_H
--- include/card.h
+++ include/card.h
@@ -1,4 +1,19 @@
 #ifndef SAPITRELLO_CSOPORT_11_CARD_H
 #define SAPITRELLO_CSOPORT_11_CARD_H
 
+typedef enum {
+    TODO, WORKING, DONE;
+} Status;
+
+typedef struct {
+    int cardID;
+
+    char* title;
+    char* description;
+    time_t timestamp;
+    Status status;
+    char* user;
+    char** userLog;
+} Card;
+
 #endif //SAPITRELLO_CSOPORT_11_CARD_H
