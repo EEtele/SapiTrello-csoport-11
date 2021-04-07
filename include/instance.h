d2e7356db0572fa1a201c17031fb09e13344ff15
void <lehelkov35@gmail.com>
2021-04-07 21:44:54 +0300

    Instance kickstart

--- include/instance.h
+++ include/instance.h
@@ -1,4 +1,22 @@
 #ifndef SAPITRELLO_CSOPORT_11_INSTANCE_H
 #define SAPITRELLO_CSOPORT_11_INSTANCE_H
 
+#include <stdlib.h>
+
+#include "user.h"
+#include "board.h"
+
+struct Instance {
+    Board* selectedBoard;   // Pointer to currently selected board, null by default
+
+    int numberOfBoards;
+    Board* boards;
+    int numberOfUsers;
+    User* users;
+} instance;
+// Only one instance per process
+
+int instanceLoad();
+int instanceWrite();
+
 #endif //SAPITRELLO_CSOPORT_11_INSTANCE_H
---
+++ src/instance.c
@@ -0,0 +1,10 @@
+#include "../include/instance.h"
+
+int instanceLoad() {
+    // Loads all boards and users from disk
+    // Creates folders if empty
+}
+
+int instanceWrite() {
+    // Writes all boards and users to disk
+}
