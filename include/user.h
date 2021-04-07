509a8b5475b45c72e624ff62ba58dbd4700c5609
void <lehelkov35@gmail.com>
2021-04-07 21:45:05 +0300

    User struct and functions

--- include/user.h
+++ include/user.h
@@ -1,4 +1,14 @@
 #ifndef SAPITRELLO_CSOPORT_11_USER_H
 #define SAPITRELLO_CSOPORT_11_USER_H
 
+#include <string.h>
+
+#include "board.h"
+#include "instance.h"
+
+typedef struct {
+    char* name;
+    char* email;
+} User;
+
 #endif //SAPITRELLO_CSOPORT_11_USER_H
---
+++ src/user.c
@@ -0,0 +1,36 @@
+#include "../include/user.h"
+
+User* userCreate(char* userName, char* userEmail) {
+    User* user = calloc(1, sizeof(User));
+    strcpy(user->name, userName);
+    strcpy(user->email, userEmail);
+
+    return user;
+}
+
+void userDelete(char* userEmail) {
+    /*
+    for (int i = 0; i < instance.numberOfBoards; i++) {
+        if (boardContainsUser(instance.boards[i].name, userEmail)) {
+            boardRemoveUser(instance.boards[i].name, userEmail);
+        }
+    }
+    */
+
+    for (int i = 0; i < instance.numberOfUsers; i++) {
+        if (strcmp(instance.users[i].email, userEmail) == 0) {
+            User* temp = instance.users[i];
+
+            for (int j = i; j < instance.numberOfUsers-1; j++) {
+                instance.users[i] = instance.users[i+1];
+            }
+
+            instance.numberOfUsers--;
+            instance.users = (User*)realloc(instance.users,
+                                            instance.numberOfUsers*sizeof(User));
+
+            free(temp);
+            return;
+        }
+    }
+}
